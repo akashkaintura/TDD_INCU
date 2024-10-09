bd09f40e46c7f7e3e08bb21cc265b5ec6a5c7f7b
akashkoco <91880623+akashkoco@users.noreply.github.com>
2024-10-09 18:53:33 +0530

    added updated version to the prod

---
+++ jest.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  extensionsToTreatAsEsm: ['.ts'],
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+};
